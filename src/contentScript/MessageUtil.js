3da967b73a2216b977924770c07efa6db6b10f7b
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-01-30 13:59:34 +0900

    Disabled logs

--- src/contentScript/MessageUtil.js
+++ src/contentScript/MessageUtil.js
@@ -2,12 +2,14 @@ const debugMode = false;
 export default class MessageUtil{
   static toBackground(message){
     chrome.runtime.sendMessage(message);
+    if(!debugMode)return;
     console.log(`ContentScript -> BackgroundPage\n${message.type}`);
     console.log(message);
   }
 
   static toWindow(wnd,message){
     wnd.postMessage(message,"*");
+    if(!debugMode)return;
     console.log(`ContentScript -> Window\n${message.type}`);
     console.log(message);
   }
